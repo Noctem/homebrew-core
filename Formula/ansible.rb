56cb6009d189c6a5ea1c066914d437e5900b023c
Alexander Bayandin <a.bayandin@gmail.com>
2019-10-15 09:04:38 +0100

    ansible: revision bump for python

--- Formula/ansible.rb
+++ Formula/ansible.rb
@@ -5,6 +5,7 @@ class Ansible < Formula
   homepage "https://www.ansible.com/"
   url "https://releases.ansible.com/ansible/ansible-2.8.5.tar.gz"
   sha256 "8e9403e755ce8ef27b6066cdd7a4c567aa80ebe2fd90d0ff8efa0a725d246986"
+  revision 1
   head "https://github.com/ansible/ansible.git", :branch => "devel"
 
   bottle do
