85be06f33e657dda085ebe189031355ab9830d7a
Alexander Bayandin <a.bayandin@gmail.com>
2019-10-15 12:15:29 +0100

    cython: revision bump for python

--- Formula/cython.rb
+++ Formula/cython.rb
@@ -3,6 +3,7 @@ class Cython < Formula
   homepage "https://cython.org/"
   url "https://files.pythonhosted.org/packages/a5/1f/c7c5450c60a90ce058b47ecf60bb5be2bfe46f952ed1d3b95d1d677588be/Cython-0.29.13.tar.gz"
   sha256 "c29d069a4a30f472482343c866f7486731ad638ef9af92bfe5fca9c7323d638e"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
