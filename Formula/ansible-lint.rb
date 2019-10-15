cb579efd5e90404da7621727860ae4fbb5a9c444
Alexander Bayandin <a.bayandin@gmail.com>
2019-10-15 09:04:50 +0100

    ansible-lint: revision bump for python

--- Formula/ansible-lint.rb
+++ Formula/ansible-lint.rb
@@ -5,7 +5,7 @@ class AnsibleLint < Formula
   homepage "https://github.com/ansible/ansible-lint/"
   url "https://files.pythonhosted.org/packages/e6/16/31f3244635b37474bbff4ac88afc916cfc24baf44766f8bcc8deeede187c/ansible-lint-4.1.0.tar.gz"
   sha256 "9430ea6e654ba4bf5b9c6921efc040f46cda9c4fd2896a99ff71d21037bcb123"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
